9dd88033daa1a0b72534c190196636ec7c3b9532
Bora <bora.arseven@gmail.com>
2023-06-11 18:37:10 +0200

    fixing code coverage

--- com.bora.airwayWareHouse/src/test/java/com/bora/airwayWareHouse/AppTest.java
+++ com.bora.airwayWareHouse/src/test/java/com/bora/airwayWareHouse/AppTest.java
@@ -2,16 +2,25 @@ package com.bora.airwayWareHouse;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.bora.airwayWareHouse.App;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
+	App app = new App();
     /**
      * Rigorous Test :-)
      */
+	@Before
+	public void setup() {
+		app = new App();
+	}
+	
     @Test
     public void shouldAnswerWithTrue()
     {
