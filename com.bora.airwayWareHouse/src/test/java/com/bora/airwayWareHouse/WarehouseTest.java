8ce13f6f52340df42d99331335d5eb07e291f814
Bora <bora.arseven@gmail.com>
2023-06-13 21:01:13 +0200

    I noticed that my warehouse has some methods that can be tested.

--- com.bora.airwayWareHouse/src/test/java/com/bora/airwayWareHouse/WarehouseTest.java
+++ com.bora.airwayWareHouse/src/test/java/com/bora/airwayWareHouse/WarehouseTest.java
@@ -1,35 +1,64 @@
 package com.bora.airwayWareHouse;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThrows;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import com.bora.airwayWareHouse.model.Airplane;
 import com.bora.airwayWareHouse.model.Warehouse;
-/**
- * Unit test for simple App.
- */
-public class WarehouseTest 
-{
-	Warehouse warehouse ;
-    /**
-     * Rigorous Test :-)
-     */
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+//testing the ability to remove and add airplanes to its Airplane list. 
+//So that after successful modification we can save our new object to the database.
+public class WarehouseTest {
+
+	@Mock
+	private List<Airplane> airplanesMock;
+
+	@InjectMocks
+	private Warehouse warehouse;
+
 	@Before
-	public void setup() {
-		warehouse = new Warehouse();
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
 	}
-	
-  
+
+	@Test
+	public void addAirplane_ShouldAddAirplaneToList() {
+		Airplane airplane = new Airplane();
+		warehouse.addAirplane(airplane);
+
+		verify(airplanesMock).add(airplane);
+		assertThat(warehouse.getAirplanes()).contains(airplane);
+	}
+
+	@Test
+	public void removeAirplane_ShouldRemoveAirplaneFromList() {
+		Airplane airplane = new Airplane();
+		airplane.setId(1);
+		warehouse.setAirplanes(new ArrayList<>(List.of(airplane)));
+
+		warehouse.removeAirplane(airplane);
+
+		verify(airplanesMock).removeIf(any());
+		assertThat(warehouse.getAirplanes()).isEmpty();
+	}
+
+	@Test
+	public void setAirplanes_ShouldSetAirplanesList() {
+		List<Airplane> airplanes = new ArrayList<>();
+		airplanes.add(new Airplane());
+		airplanes.add(new Airplane());
+
+		warehouse.setAirplanes(airplanes);
+
+		assertThat(warehouse.getAirplanes()).containsExactlyElementsOf(airplanes);
+	}
 }
